a82b72678bd547467b9f7486a3b0d9bc13a4448f
gabriel <gabrielsil20177@gmail.com>
2024-12-22 16:51:58 -0300

    update postman ci/cd and app.js

--- app.js
+++ app.js
@@ -30,7 +30,6 @@
             process.exit(1); // Interrompe a execução se a URI não estiver definida
         }
     // Mongoose 
-    console.log(mongoURI)
         mongoose.connect(mongoURI) // esse mongouri determina se vai conectar pelo local ou pelo server
             .then(() => { console.log('Mongo Connected'); })
             .catch((err) => { console.log('An error occurred when trying to connect to the server: ' + err); })
