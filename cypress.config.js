bf89466bd9c636e1b502708cce97772e40fcf5c0
gabriel <gabrielsil20177@gmail.com>
2025-02-01 18:39:27 -0300

    fix configuracao

--- cypress.config.js
+++ cypress.config.js
@@ -2,11 +2,6 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
-      require('cypress-mochawesome-reporter/plugin')(on);
-      return config;
-    },
     projectId: "19uf39",
     pageLoadTimeout: 30000, 
     video: false
